b7ff7b33b0af4e3fafcbc9e67231e115e2cb0fc9
Tuan Phung <tuan@phung.cz>
2022-10-28 15:19:20 +0200

    added twitter og

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,10 +11,26 @@ export default function Document() {
           content="An open source about.me like application built using Next.js 13 and Stacks.js."
         />
         <meta property="og:url" content="https://linkstacks.vercel.app" key="ogurl" />
-        <meta property="og:image" content="https://linkstacks.vercel.app/preview.png" key="ogimage" />
+        <meta
+          property="og:image"
+          content="https://linkstacks.vercel.app/preview.png"
+          key="ogimage"
+        />
         <meta property="og:site_name" content="Linkstacks" key="ogsitename" />
         <meta property="og:title" content="Linkstacks.verlce.app" key="ogtitle" />
-        <meta property="og:description" content="An open source about.me like application built using Next.js 13 and Stacks.js." key="ogdesc" />
+        <meta
+          property="og:description"
+          content="An open source about.me like application built using Next.js 13 and Stacks.js."
+          key="ogdesc"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="linkstacks.vercel.app" />
+        <meta name="twitter:title" content="linkstacks.vercel.app" />
+        <meta name="twitter:description" content="An open source about.me like application built using Next.js 13 and Stacks.js." />
+        <meta name="twitter:creator" content="@Gr8087" />
+        <meta property="og:url" content='https://linkstacks.vercel.app' />
+        <meta name="twitter:image" content="https://linkstacks.vercel.app/preview.png" />
       </Head>
 
       <body>
